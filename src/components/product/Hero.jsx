2e9552f7cc9557b4d65ce71beca65d6bc5731944
Chirag Goyal <chiraggoyal1910@gmail.com>
2023-09-02 17:21:03 +0530

    Remove wishlist from product page

--- src/components/product/Hero.jsx
+++ src/components/product/Hero.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { faker } from "@faker-js/faker";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
@@ -28,12 +27,6 @@ const Hero = ({ product }) => {
     const handleBuyNow = () => {};
 
     const handleAddToCart = () => {};
-
-    const handleAddFavourite = () => {};
-
-    const isFavourite = () => {
-        return faker.datatype.boolean();
-    };
 
     return (
         product.id && (
@@ -224,25 +217,6 @@ const Hero = ({ product }) => {
                                         onClick={handleAddToCart}
                                     >
                                         Add to Cart
-                                    </button>
-                                    <button
-                                        className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500"
-                                        onClick={handleAddFavourite}
-                                    >
-                                        <svg
-                                            fill={
-                                                isFavourite()
-                                                    ? "red"
-                                                    : "currentColor"
-                                            }
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            className="w-5 h-5"
-                                            viewBox="0 0 24 24"
-                                        >
-                                            <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                                        </svg>
                                     </button>
                                 </div>
                             </div>
